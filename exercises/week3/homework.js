54dc0369336ad9b97adfb6d66659aa2450c9b040
mario-alfonso <macortez75@gmail.com>
2020-06-07 14:56:55 -0400

    week3 homework

--- exercises/week3/homework.js
+++ exercises/week3/homework.js
@@ -75,17 +75,32 @@ test(toLowerCase('z'), 'z', "toLower 6")
 //exercise 4
 //print the array using a for loop
 function printArray(myArray){
-    //use a for loop to print the array
+    for(let i=0; i < myArray.length; i++){
+        console.log(myArray[i])
+    }
 }
+
+console.log("**** Printing an array *********")
+printArray(['The Matrix', 'King Kong', 'Jumanji', 'Secret Life of Pets'])
 
 //exercise 5
 //Print the array as a string
 //do not change the array but print it as a string
 //do not use any array methods
 function printArrayAsString(myArray){
-    
+
+    let myArrayToString = "";
+    for(let i=0; i < (myArray.length - 1); i++){
+        myArrayToString += myArray[i] + ', '
+    }
+    myArrayToString += myArray[(myArray.length - 1 )]
+
+    console.log( myArrayToString )
 }
 
+
+console.log("**** Printing an array as string *********")
+printArrayAsString(['The Matrix', 'King Kong', 'Jumanji', 'Secret Life of Pets'])
 
 //exercise 6
 //assume you have a non-empty array
@@ -96,6 +111,15 @@ function printArrayAsString(myArray){
 //
 function convertArrayToString(myArray){
 
+    let myArrayToString = "";
+    if ( myArray.length > 1 ) {
+        for(let i=0; i < (myArray.length - 1); i++){
+            myArrayToString += myArray[i] + ','
+        }    
+    }
+    myArrayToString += myArray[(myArray.length - 1 )]
+
+    return myArrayToString;
 }
 
 test(convertArrayToString([1,2,3,4,5]), '1,2,3,4,5', "convertArrayToString 1")
@@ -105,7 +129,7 @@ test(convertArrayToString([1]), '1', "convertArrayToString 2")
 //exercise 7
 //return the lastElement in the array
 function getLastElement(myArray){
-
+    return myArray[(myArray.length - 1 )]
 }
 test(getLastElement([1]), 1, "getLastElement 1")
 test(getLastElement([1,2,,3]), 3, "getLastElement 2")
@@ -118,7 +142,21 @@ test(getLastElement([1,2,,3]), 3, "getLastElement 2")
 //assume the values are all integers
 //hit use for loops
 function containsValue(myArray, value){
-
+    console.log( value );
+    let valueToCompare = NaN;
+    if ( Number.isInteger(value)) {
+        valueToCompare = value;     
+    }
+    console.log( valueToCompare );
+    let valueFound = false;
+    for(let i=0; i < myArray.length ; i++){
+        if ( myArray[i] != null) {
+            if ( valueToCompare == myArray[i] ) {
+                valueFound = true;
+            }
+        }
+    }
+    return valueFound;
 }
 test(containsValue([1,2,3], 1), true, "containsValue 1")
 test(containsValue([1,2,,3], 2), true, "containsValue 2")
@@ -134,7 +172,9 @@ test(containsValue([1,2,,3], '2'), false, "containsValue 5")
 //assume the target array has a legnth of
 //zero
 function copyArray(myArray, targetArray){
-
+    for(let i=0; i < myArray.length ; i++){
+        targetArray.push( myArray[i] );
+    }
 }
 
 let a1 = [1,3,5,8]
@@ -147,6 +187,14 @@ test(a2[3], 8, "copyArray 2")
 //write a loops that prints all
 //the numbers from 0 to 1000
 //only print the numbers divisible by 30
+function printDiv30() {
+    for(let i=0; i < 1000 ; i++){
+        if ( i % 30 == 0 ) {
+            console.log( i );
+        }
+    }
+}
+printDiv30();
 
 //write a for loop that prints the following
 //shape
@@ -154,12 +202,40 @@ test(a2[3], 8, "copyArray 2")
 //    * *
 //    * * *
 //    * * * * 
+function printHorGraph( lastElement ) {  
+    for(let i= 0; i < lastElement; i++) {
+        let asterisk = "";
+        for(let j=0+1; j<=i;j++) {
+            asterisk += "*";
+        }
+        console.log(asterisk);
+    }    
+}
+printHorGraph(5);
 
 //write a for loop that prints the following shape
 //        *
 //       ***
 //      *****
 //     *******
+function printMidGraph( lastElement ) {  
+    
+    for(let i= 0; i < lastElement; i++) {
+        let asterisk = "";
+        let doPrint = false;
+        let spaces = "";
+        for(let j=0+1; j<=i;j++) {
+            if ( i % 2 != 0 ) {
+                doPrint = true;
+                asterisk += "*";
+            }  
+        }
+        if ( doPrint ) {
+            console.log(asterisk);
+        }        
+    }    
+}
+printMidGraph(9);
 
 //exercise 10
 //BONUS points
@@ -171,15 +247,21 @@ test(a2[3], 8, "copyArray 2")
 //hint use the mod operator %
 //for integers not divisible by either 3 or 5 return empty string
 function fizzBuzz(val){
-
+    let returnedValue = "";
+    if ( val % 3 == 0 && val % 5 != 0 ) {
+        returnedValue += "fizz";
+    } else if ( val % 5 == 0 && val % 3 != 0 ) {
+        returnedValue += "buzz"; 
+    } else if ( val % 3 == 0 && val % 5 == 0 ) {
+        returnedValue += "fizz buzz";
+    }
+    return returnedValue;
 }
 
 test(fizzBuzz(3), "fizz", "fizzBuzz 1")
 test(fizzBuzz(5), "buzz", "fizzBuzz 2")
 test(fizzBuzz(15), "fizz buzz", "fizzBuzz 3")
 test(fizzBuzz(2), "", "fizzBuzz 4")
-
-
 
 
 
